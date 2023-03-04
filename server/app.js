95365b524cfc5f63f9368877f01a5ca55ab35ba6
Shrideep Pakhare <114938473+ShrideepP@users.noreply.github.com>
2023-03-04 09:14:49 +0530

    removed import of an unnecessary file

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const fs = require("node:fs");
 
 const app = express();
 const routes = require("./router/router");
